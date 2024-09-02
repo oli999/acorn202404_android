a12a8e5e94177c077e02d8c12fc3f8a3eb89850d
kimgura <oli999@naver.com>
2024-09-02 11:01:29 +0900

    RestApiClient 추가함

--- step07customlistview/src/main/java/com/example/step07customlistview/MainActivity.kt
+++ step07customlistview/src/main/java/com/example/step07customlistview/MainActivity.kt
@@ -53,14 +53,21 @@ class MainActivity : AppCompatActivity() {
             val addr=inputAddr.text.toString()
             //Post 방식으로 서버에 전송하기
             lifecycleScope.launch(Dispatchers.Main) {
-                val result = addMember(name, addr)
+                //입력한 이름과 주소를 json 문자열로 만든다.
+                val json="""{
+                        "name":"$name",
+                        "addr":"$addr"
+                    }""".trimMargin()
+                //RestApiClient 유틸리티를 이용해서 서버에 전송하고 응답된 문자열 읽어오기
+                val result = RestApiClient.post("http://192.168.0.181:8888/members", json)
+                //응답된 문자열을 Toast 메세지로 출력해보기
                 Toast.makeText(this@MainActivity, result, Toast.LENGTH_SHORT).show()
             }
         }
 
         // http://192.168.0.181:8888/members 요청을 해서 응답되는 json 문자열을 Log 창에 출력해 보세요
         lifecycleScope.launch(Dispatchers.Main) {
-            val result = getMember()
+            val result = RestApiClient.get("http://192.168.0.181:8888/members")
             Log.d("MaiActivity", result)
             //응답받은 문자열의 형식이 [{},{},{}...] 이기 때문에 해당 문자열을 이용해서 JSONArray 객체를 생성
             val array=JSONArray(result)
@@ -80,96 +87,6 @@ class MainActivity : AppCompatActivity() {
             //반복문 돌고나서 Adapter 에 데이터가 수정되었다고 알린다. (UI 업데이트를 위해)
             adapter.notifyDataSetChanged()
         }
-    }
-
-    //이름과 주소를 post 방식  /members  요청을 하면서 요청의 body 에 json 문자열 전송하기
-    suspend fun addMember(name:String, addr:String):String{
-
-        val result= withContext(Dispatchers.IO){
-            //문자열을 누적할 객체
-            val builder = StringBuilder()
-
-            try {
-                //요청 url 을 생성자의 인자로 전달하면서 URL 객체를 생성한다
-                val url = URL("http://192.168.0.181:8888/members")
-                //URLConnection 객체를 원래 type (자식 type) 으로 casting 해서 받는다.
-                val conn = url.openConnection() as HttpURLConnection
-                if (conn != null) {
-                    conn.connectTimeout = 20000 //응답을 기다리는 최대 대기 시간
-                    conn.requestMethod = "POST" // 요청 메소드 설정 (Default 는 GET)
-                    conn.useCaches = false //케쉬 사용 여부
-                    conn.setRequestProperty("Content-Type","application/json")
-                    //문자열을 출력하기 위한 객체
-                    val osw=OutputStreamWriter(conn.outputStream)
-                    osw.write("""
-                        {
-                            "name":"$name",
-                            "addr":"$addr"
-                        }
-                    """.trimIndent())
-                    osw.flush()
-                    //응답 코드를 읽어온다.
-                    val responseCode = conn.responseCode
-                    if (responseCode == HttpURLConnection.HTTP_OK) { //정상 응답이라면 (200)
-                        //문자열을 읽어들일수 있는 객체의 참조값 얻어오기
-                        val br =
-                            BufferedReader(InputStreamReader(conn.inputStream))
-                        //반복문 돌면서
-                        while (true) {
-                            //문자열을 한줄씩 읽어 들인다.
-                            val line = br.readLine() ?: break
-                            //StringBuilder 객체에 누적 시키기
-                            builder.append(line)
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                Log.e("MainActivity", e.message!!)
-            } //try~catch
-
-            // 여기서 리턴되는 문자열이 withContext(){} 의 리턴 데이터가 된다.
-            builder.toString()
-        }
-        return result
-    }
-
-    suspend fun getMember():String{
-        val result= withContext(Dispatchers.IO){
-            //문자열을 누적할 객체
-            val builder = StringBuilder()
-
-            try {
-                //요청 url 을 생성자의 인자로 전달하면서 URL 객체를 생성한다
-                val url = URL("http://192.168.0.181:8888/members")
-                //URLConnection 객체를 원래 type (자식 type) 으로 casting 해서 받는다.
-                val conn = url.openConnection() as HttpURLConnection
-                if (conn != null) {
-                    conn.connectTimeout = 20000 //응답을 기다리는 최대 대기 시간
-                    conn.requestMethod = "GET" // 요청 메소드 설정 (Default 는 GET)
-                    conn.useCaches = false //케쉬 사용 여부
-                    //응답 코드를 읽어온다.
-                    val responseCode = conn.responseCode
-                    if (responseCode == HttpURLConnection.HTTP_OK) { //정상 응답이라면 (200)
-                        //문자열을 읽어들일수 있는 객체의 참조값 얻어오기
-                        val br =
-                            BufferedReader(InputStreamReader(conn.inputStream))
-                        //반복문 돌면서
-                        while (true) {
-                            //문자열을 한줄씩 읽어 들인다.
-                            val line = br.readLine() ?: break
-                            //StringBuilder 객체에 누적 시키기
-                            builder.append(line)
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                Log.e("MainActivity", e.message!!)
-            } //try~catch
-
-            // 여기서 리턴되는 문자열이 withContext(){} 의 리턴 데이터가 된다.
-            builder.toString()
-        }
-        return result
     }
 }
 
